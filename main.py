0471990efc57855af285e797e98d1080d54b4e00
Job <job@aaa.ru>
2024-04-16 17:09:19 +0500

    1job

--- main.py
+++ main.py
@@ -9,9 +9,35 @@ app = Flask('__name__')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JSON_AS_ASCII'] = True
 app.config['DEBUG'] = True
 
+api = Api(app)
+api.app.config['RESTX_JSON'] = {'ensure_ascii': False, 'indent': 4}
+movies_ns = api.namespace('movies')
+
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+@movies_ns.route('/')
+class MoviesView(Resource):
+    def get(self):
+        genre_id = request.args.get('genre_id')
+        director_id = request.args.get('director_id')
+        movies = Movie.query
+        if genre_id:
+            movies = movies.filter(genre_id == Movie.genre_id)
+        if director_id:
+            movies = movies.filter(director_id == Movie.director_id)
+        movies = movies.all()
+        return (movies_schema.dump(movies), 200)
+
+
+@movies_ns.route('/<int:id>')
+class MoviesView(Resource):
+    def get(self, id: int):
+        return movie_schema.dump(Movie.query.get(id)), 200
 
 
 class Movie(db.Model):
@@ -36,7 +62,7 @@ class Director(db.Model):
 
 
 class Genre(db.Model):
-    __tablename__ = 'genre'
+    __tablename__ = 'gener'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
 
@@ -52,7 +78,7 @@ class MovieSchema(Schema):
     director_id = fields.Int()
 
 
-# movie_schema = MovieSchema()
+movie_schema = MovieSchema()
 movies_schema = MovieSchema(many=True)
 with app.app_context():
     all_movies = Movie.query.all()
@@ -61,5 +87,4 @@ with app.app_context():
     print(movies_schema)
 
 if __name__ == '__main__':
-
     app.run()
